544c58bf36019b96a7a297f256dfc7647c9b6362
Colseph <cole.m.richardson.mail@gmail.com>
2019-03-08 16:04:29 -0700

    initial for lib2mp3.sh

---
+++ lib2mp3.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+#~encodes existting library to mp3
+#~TODO add flag for extension(figure out metadata mapping for different formats?) 
+#~so user can convert to any almost any format
+
+#~ config ~#
+
+
+#~ functions ~#
+
+_help() {
+    echo "
+    usage:
+    --help show this message
+    "
+}
+
+
+#~ script_start ~#
+
+while [ "$1" != "" ]; do
+    case $1 in
+        *help)
+            _help
+            exit
+           ;;
+       -b)
+           shift
+           bitrate="$1"
+           ;;
+       -s)
+           shift
+           sourceDir="$1"
+           ;;
+       -d)
+           shift
+           destDir="$1"
+           ;;
+       *)
+           echo "unknown option: '$1'"
+           _help
+           exit 1
+           ;;
+   esac
+   shift
+done
+#defaults
+bitrate="${bitrate:-320K}"
+sourceDir="${sourceDir:-.}"
+destDir="${destDir:-./mp3_converted}"
+
+echo source:$sourceDir
+echo dest:$destDir
+echo bitrate:$bitrate
+
+#~re-create directory structure
+find "$sourceDir" -type d -not -path "*$destDir*" -exec bash -c 'destPath="$2/${1#*"$3"}"; echo "item:$destPath"; mkdir -p "$destPath"' - {} "$destDir" "$sourceDir" \;
+
+#~encode files
+find "$sourceDir" -type f -not -path "*$destDir*" -exec bash -c 'destPath="$2/${1#*"$3"}"; echo "item:$destPath"; [[ "$1" == *".mp3" ]] && cp "$1" "$destPath" || ffmpeg -v quiet -stats -i "$1" -ab "$bitrate" -map_metadata 0 -id3v2_version 3 "${destPath%.*}.mp3"' - {} "$destDir" "$sourceDir" \;
+echo Done.
