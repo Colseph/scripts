02d448b95d99b69ef25f60f30563b5eb81d5743c
Colseph <36282369+Colseph@users.noreply.github.com>
2019-04-16 21:22:59 -0600

    initial commit for mkvMux.sh

---
+++ mkvMux.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#~MKV muxer/tagger
+#~requires mkvtoolnix(might attempt ffmpeg ver later?)
+#~Author - LuX/\er (LXR)
+#~Version 0.1 (Victor)
+#~Changelog -
+#~v0.1(Victor) - Initial Write-up
+
+#~~~~~~~~~~#
+#~ Config ~#
+#~~~~~~~~~~#
+DEPENDENCIES=(mkmerge mkvextract mkvpropedit mkvinfo)
+
+#~~~~~~~~~~~~~#
+#~ Functions ~#
+#~~~~~~~~~~~~~#
+
+#~~~~~~~~~~~~~~~~#
+#~ Script Start ~#
+#~~~~~~~~~~~~~~~~#
